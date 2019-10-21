0c441bed6830fdf042c32fdd8a9362cdc44dd8fa
Trăistaru Vlad-Viorel <vladtherocker@gmail.com>
2019-10-21 15:17:47 +0300

    class restructuring, testing object functionality

--- pynotes.py
+++ pynotes.py
@@ -1,75 +1,122 @@
 import pygame
+import copy
 
-def redraw_all():
-	screen.fill((0,0,0))
-	for i in range(note_rows):
-		for j in range(note_columns):
-			if note_current > (i)*note_columns + j:
-				screen.blit(note_image, (j*note_image.get_width(), i*note_image.get_height()))
-	
-	screen.blit(plus_button_image, (0, display_y - plus_button_image.get_height()))
-	screen.blit(minus_button_image, (plus_button_image.get_width(), display_y - minus_button_image.get_height()))
-	screen.blit(one_button_image, (plus_button_image.get_width() + minus_button_image.get_width() + 5, display_y - one_button_image.get_height()))
+class Button:
+	def __init__(self, buttonImage, buttonRect):
+		self.image = buttonImage
+		self.rect = buttonRect
 
-	plus_button_image.get_rect().x = 0
-	plus_button_image.get_rect().y = display_y - plus_button_image.get_height()
+	def reset(self, buttonImage, buttonRect):
+		self.__init__(buttonImage, buttonRect)
 
+class Image:
+	def __init__(self, imagePath, imageScaleWidth = 1, imageScaleHeight = 1):
+		self.path = imagePath
+		self.scaleW = imageScaleWidth
+		self.scaleH = imageScaleHeight
+		self.setup()
+
+	def setup(self):
+		self.image = pygame.image.load(self.path)
+		self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*self.scaleW), int(self.image.get_height()*self.scaleH)))
+		self.set_rect(0, 0)
+
+	def reset(self, imagePath, imageScaleWidth = 1, imageScaleHeight = 1):
+		self.__init__(imagePath, imageScaleWidth, imageScaleHeight)
+
+	def set_rect(self, imageX = 0, imageY = 0):
+		self.rect = pygame.Rect(imageX, imageY, self.image.get_width(), self.image.get_height())
+
+	def set_scale(self, imageScaleWidth = 1, imageScaleHeight = 1):
+		self.scaleW = imageScaleWidth
+		self.scaleH = imageScaleHeight
+		self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*self.scaleW), int(self.image.get_height()*self.scaleH)))
+
+class Window:
+	def __init__(self, windowWidth = 1280, windowHeight = 720, windowName = "pynotes", windowResizable = True, windowBackgroundColor = (0,0,0)):
+		self.width = windowWidth
+		self.height = windowHeight
+		self.name = windowName
+		self.resizable = windowResizable
+		self.backgroundColor = windowBackgroundColor
+		self.setup()
+
+	def setup(self):
+		pygame.display.set_caption(self.name)
+		if self.resizable:
+			self.display = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
+		else:
+			self.display = pygame.display.set_mode((self.width, self.height))
+		self.fill(self.backgroundColor)
+
+	def reset(self, windowWidth = 1280, windowHeight = 720, windowName = "pynotes", windowResizable = True, windowBackgroundColor = (0,0,0)):
+		self.__init__(windowWidth, windowHeight, windowName, windowResizable, windowBackgroundColor)
+
+	def fill(self, windowBackgroundColor = (0,0,0)):
+		self.backgroundColor = windowBackgroundColor
+		self.display.fill(self.backgroundColor)
+
+	def draw(self, drawable, drawableX, drawableY):
+		self.display.blit(drawable, (drawableX, drawableY))
+
+	def draw_image(self, image):
+		self.display.blit(image.image, (image.rect.left, image.rect.top))
+
+def init():
+	try:
+		pygame.init()
+		return True
+	except:
+		return False
+
+def update():
 	pygame.display.flip()
 
+def main():
+	running = init()
+	if running:
+		window = Window()
+		images = {"note":Image("postitnote.png",1/6,1/6), "add":Image("plusbutton.png",1/2,1/2), "remove":Image("minusbutton.png",1/5,1/5)}
+
+		note_rows = 5
+		note_columns = 7
+		note_current = 24
+
+		note_images = []
+
+		for i in range(note_current):
+			note_images.append(Image("postitnote.png",1/6,1/6))
+
+		for i in range(note_rows):
+			for j in range(note_columns):
+				note_index = i * note_columns + j
+				if note_current > note_index:
+					note_images[note_index].set_rect(j * note_images[note_index].image.get_width(), i * note_images[note_index].image.get_height())
+					window.draw_image(note_images[note_index])
+
+		images["add"].set_rect(0, window.height - images["add"].image.get_height())
+		window.draw_image(images["add"])
+
+		images["remove"].set_rect(images["add"].image.get_width(), window.height - images["remove"].image.get_height())
+		window.draw_image(images["remove"])
+
+		update()
+
+		while running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					running = False
+				if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+					if images["add"].rect.collidepoint(event.pos):
+						print("add")
+					elif images["remove"].rect.collidepoint(event.pos):
+						print("remove")
+					else:
+						for image_index in range(len(note_images)):
+							if note_images[image_index].rect.collidepoint(event.pos):
+								print(image_index + 1)
+	else:
+		print("pygame init failed!")
+
 if __name__ == "__main__":
-	pygame.init()
-	pygame.display.set_caption("pynotes")
-	
-	display_x = 1280
-	display_y = 720
-
-	screen = pygame.display.set_mode((display_x, display_y), pygame.RESIZABLE)
-
-	running = True
-
-	note_image = pygame.image.load("postitnote.png").convert_alpha()
-	note_image_scale = 1/6
-	note_image = pygame.transform.scale(note_image, (int(note_image.get_width()*note_image_scale), int(note_image.get_height()*note_image_scale)))
-
-	plus_button_image = pygame.image.load("plusbutton.png").convert_alpha()
-	plus_button_image_scale = 1/2
-	plus_button_image = pygame.transform.scale(plus_button_image, (int(plus_button_image.get_width()*plus_button_image_scale), int(plus_button_image.get_height()*plus_button_image_scale)))
-	plus_button_rect = pygame.Rect(0, display_y - plus_button_image.get_height(), plus_button_image.get_width(), plus_button_image.get_height())
-
-	minus_button_image = pygame.image.load("minusbutton.png")
-	minus_button_image_scale = 1/5
-	minus_button_image = pygame.transform.scale(minus_button_image, (int(minus_button_image.get_width()*minus_button_image_scale), int(minus_button_image.get_height()*minus_button_image_scale)))
-	minus_button_rect = pygame.Rect(plus_button_image.get_width(), display_y - minus_button_image.get_height(), minus_button_image.get_width(), minus_button_image.get_height())
-
-	one_button_image = pygame.image.load("onebutton.png")
-	one_button_image_scale = 1/5
-	one_button_image = pygame.transform.scale(one_button_image, (int(one_button_image.get_width()*one_button_image_scale), int(one_button_image.get_height()*one_button_image_scale)))
-	one_button_rect = pygame.Rect(plus_button_image.get_width() + minus_button_image.get_width() + 5, display_y - one_button_image.get_height(), one_button_image.get_width(), one_button_image.get_height())
-
-	note_rows = 4
-	note_columns = 6
-	note_current = 1
-	
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-			if event.type == pygame.VIDEORESIZE:
-				display_x = event.w
-				display_y = event.h
-				screen = pygame.display.set_mode((display_x, display_y), pygame.RESIZABLE)
-				redraw_all()
-			if event.type == pygame.MOUSEBUTTONUP:
-				if event.button == 1:
-					if plus_button_rect.collidepoint(event.pos):
-						note_current += 1
-						redraw_all()
-					if minus_button_rect.collidepoint(event.pos):
-						note_current -= 1
-						redraw_all()
-					if one_button_rect.collidepoint(event.pos):
-						note_current = 1
-						redraw_all()
-				'''print(screen.get_width(), screen.get_height())'''
-
-	pygame.quit()
+	main()
