cea2c79a0ab3db871415d461198b75c8441b7237
Trăistaru Vlad-Viorel <vladtherocker@gmail.com>
2019-10-22 21:48:31 +0300

    interface class and some loop restructuring

--- pynotes.py
+++ pynotes.py
@@ -1,13 +1,61 @@
 import pygame
-import copy
 
-class Button:
-	def __init__(self, buttonImage, buttonRect):
-		self.image = buttonImage
-		self.rect = buttonRect
+class Interface:
 
-	def reset(self, buttonImage, buttonRect):
-		self.__init__(buttonImage, buttonRect)
+	current_interface = ""
+
+	def __init__(self, windowObject, interfaceName):
+		self.windowObject = windowObject
+		self.interfaceName = interfaceName
+
+	def set_curr_interface(self):
+		Interface.current_interface = self.interfaceName
+
+	def init_draw(self):
+		getattr(Interface, self.interfaceName + "_init")(self)
+		self.draw()
+		self.set_curr_interface()
+
+	def draw(self):
+		getattr(Interface, self.interfaceName)(self)
+		self.set_curr_interface()
+
+	def main_interface_init(self):
+		self.images = {"note":Image("postitnote.png",1/6,1/6), "add":Image("plusbutton.png",1/2,1/2), "remove":Image("minusbutton.png",1/5,1/5)}
+
+		self.note_rows = 5
+		self.note_columns = 7
+		self.note_current = 20
+
+	def main_interface(self):
+		self.note_images = []
+
+		for i in range(self.note_current):
+			self.note_images.append(Image("postitnote.png",1/6,1/6))
+
+		for i in range(self.note_rows):
+			for j in range(self.note_columns):
+				note_index = i * self.note_columns + j
+				if self.note_current > note_index:
+					self.note_images[note_index].set_rect(j * self.note_images[note_index].image.get_width(), i * self.note_images[note_index].image.get_height())
+					self.windowObject.draw_image(self.note_images[note_index])
+
+		self.images["add"].set_rect(0, self.windowObject.height - self.images["add"].image.get_height())
+		self.windowObject.draw_image(self.images["add"])
+
+		self.images["remove"].set_rect(self.images["add"].image.get_width(), self.windowObject.height - self.images["remove"].image.get_height())
+		self.windowObject.draw_image(self.images["remove"])
+
+	def note_interface_init(self):
+		font = pygame.font.Font('freesansbold.ttf', 32)
+		text = font.render('dinamo', True, (255,255,255), (0,0,0))
+		textRect = text.get_rect()
+		textRect.center = (self.windowObject.width // 2, self.windowObject.height // 2)
+		self.windowObject.display.blit(text, textRect)
+
+	def note_interface(self):
+		pass
+
 
 class Image:
 	def __init__(self, imagePath, imageScaleWidth = 1, imageScaleHeight = 1):
@@ -31,6 +79,9 @@ class Image:
 		self.scaleW = imageScaleWidth
 		self.scaleH = imageScaleHeight
 		self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*self.scaleW), int(self.image.get_height()*self.scaleH)))
+
+	def collide_point(self, position):
+		return self.rect.collidepoint(position)
 
 class Window:
 	def __init__(self, windowWidth = 1280, windowHeight = 720, windowName = "pynotes", windowResizable = True, windowBackgroundColor = (0,0,0)):
@@ -69,52 +120,45 @@ def init():
 	except:
 		return False
 
-def update():
+def update_all():
 	pygame.display.flip()
 
 def main():
 	running = init()
 	if running:
 		window = Window()
-		images = {"note":Image("postitnote.png",1/6,1/6), "add":Image("plusbutton.png",1/2,1/2), "remove":Image("minusbutton.png",1/5,1/5)}
 
-		note_rows = 5
-		note_columns = 7
-		note_current = 24
+		mainInterface = Interface(window, "main_interface")
+		mainInterface.init_draw()
 
-		note_images = []
+		noteInterface = Interface(window, "note_interface")
 
-		for i in range(note_current):
-			note_images.append(Image("postitnote.png",1/6,1/6))
-
-		for i in range(note_rows):
-			for j in range(note_columns):
-				note_index = i * note_columns + j
-				if note_current > note_index:
-					note_images[note_index].set_rect(j * note_images[note_index].image.get_width(), i * note_images[note_index].image.get_height())
-					window.draw_image(note_images[note_index])
-
-		images["add"].set_rect(0, window.height - images["add"].image.get_height())
-		window.draw_image(images["add"])
-
-		images["remove"].set_rect(images["add"].image.get_width(), window.height - images["remove"].image.get_height())
-		window.draw_image(images["remove"])
-
-		update()
+		update_all()
 
 		while running:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					running = False
-				if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-					if images["add"].rect.collidepoint(event.pos):
-						print("add")
-					elif images["remove"].rect.collidepoint(event.pos):
-						print("remove")
-					else:
-						for image_index in range(len(note_images)):
-							if note_images[image_index].rect.collidepoint(event.pos):
-								print(image_index + 1)
+				if Interface.current_interface == "main_interface":
+					if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+						if mainInterface.images["add"].collide_point(event.pos):
+							window.fill()
+							mainInterface.note_current += 1
+							mainInterface.draw()
+							update_all()
+							print("add")
+						if mainInterface.images["remove"].collide_point(event.pos):
+							window.fill()
+							mainInterface.note_current -= 1
+							mainInterface.draw()
+							update_all()
+							print("remove")
+						for note_index in range(len(mainInterface.note_images)):
+							if mainInterface.note_images[note_index].collide_point(event.pos):
+								window.fill()
+								noteInterface.init_draw()
+								update_all()
+
 	else:
 		print("pygame init failed!")
 
